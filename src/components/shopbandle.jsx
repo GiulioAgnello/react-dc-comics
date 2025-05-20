006cfb350e1627b7102093ecb619f40a9c0aeaf1
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-20 18:32:41 +0200

    bandle

--- src/components/shopbandle.jsx
+++ src/components/shopbandle.jsx
@@ -1,28 +1,30 @@
 export default function Shopbandle() {
   return (
-    <div id="bandle" className="container">
-      <ul>
-        <li>
-          <img src="#" alt="" />
-          <a href="#"></a>COMIC CON
-        </li>
-        <li>
-          <img src="#" alt="" />
-          <a href="#"></a>COMIC CON
-        </li>
-        <li>
-          <img src="#" alt="" />
-          <a href="#"></a>COMIC CON
-        </li>
-        <li>
-          <img src="#" alt="" />
-          <a href="#"></a>COMIC CON
-        </li>
-        <li>
-          <img src="#" alt="" />
-          <a href="#"></a>COMIC CON
-        </li>
-      </ul>
+    <div id="bandle">
+      <div className="container">
+        <ul>
+          <li>
+            <img src="buy-comics-digital-comics.png" alt="digital" />
+            <a href="#"></a>DIGITAL COMICS
+          </li>
+          <li>
+            <img src="\buy-comics-merchandise.png" alt="" />
+            <a href="#"></a>DC MERCHANDISE
+          </li>
+          <li>
+            <img src="\buy-comics-subscriptions.png" alt="" />
+            <a href="#"></a>SUBSCRIPTION
+          </li>
+          <li>
+            <img src="\buy-comics-shop-locator.png" alt="" />
+            <a href="#"></a>COMIC SHOP LOCATOR
+          </li>
+          <li>
+            <img src="\buy-dc-power-visa.svg" alt="" />
+            <a href="#"></a>DC POWER VISA
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
