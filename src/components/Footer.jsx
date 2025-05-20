73c864dbbc4c277509fa95f1ebece4a740fe1d69
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-20 19:27:06 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,123 @@
 export default function Footer() {
-  return;
+  return (
+    <footer>
+      <div className="footer-top">
+        <div className="container">
+          <div className="links-footer">
+            <ul>
+              <li>
+                <h4>DC COMICS</h4>
+              </li>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Gamers</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <h4>SHOP</h4>
+              </li>
+              <li>
+                <a href="#">Shop DC</a>
+              </li>
+              <li>
+                <a href="#">Shop DC Collectibles</a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <h4>DC</h4>
+              </li>
+              <li>
+                <a href="#">Terms Of Use</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy (news)</a>
+              </li>
+              <li>
+                <a href="#">Ad Choices</a>
+              </li>
+              <li>
+                <a href="#">Advertising</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Subscription</a>
+              </li>
+              <li>
+                <a href="#">Talent Workshops</a>
+              </li>
+              <li>
+                <a href="#">CPSC Certificates</a>
+              </li>
+              <li>
+                <a href="#">Ratings</a>
+              </li>
+              <li>
+                <a href="#">Shop Help</a>
+              </li>
+              <li>
+                <a href="#">contanct Us</a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <h4>SITES</h4>
+              </li>
+              <li>
+                <a href="#">DC</a>
+              </li>
+              <li>
+                <a href="#">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="#">DC Kids</a>
+              </li>
+              <li>
+                <a href="#">DC Universe</a>
+              </li>
+              <li>
+                <a href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+          <div className="links-photo">
+            <img src="\dc-logo-bg.png" alt="" />
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="container">
+            <div className="footer-left">
+              <button>
+                <a href="">SIGN-UP NOW!</a>
+              </button>
+            </div>
+            <div className="footer-right">immgini</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
