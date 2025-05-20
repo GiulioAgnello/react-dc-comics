17cc60cca533c24d6962ea008486b2527715fd18
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-20 16:56:24 +0200

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div className="col-30">
+          <figure>
+            <img src="public\dc-logo.png" alt="logo header" />
+          </figure>
+        </div>
+        <div className="col-70">
+          <ul>
+            <li>
+              <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+              <a href="#">COMICS</a>
+            </li>
+            <li>
+              <a href="#">MOVIES</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">GAMES</a>
+            </li>
+            <li>
+              <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+              <a href="#">VIDEOS</a>
+            </li>
+            <li>
+              <a href="#">FANS</a>
+            </li>
+            <li>
+              <a href="#">NEWS</a>
+            </li>
+            <li>
+              <a href="#">SHOP</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
