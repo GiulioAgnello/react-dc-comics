69f5f14ec6cbd029b2d3ecf0367e8d10e504163d
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-22 15:12:51 +0200

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Header from "./components/components layout/Header";
-import Main from "./components/components layout/Main";
-import Shopbandle from "./components/components layout/shopbandle";
-import Footer from "./components/components layout/Footer";
+import Header from "./components/components_layout/Header";
+import Main from "./components/components_layout/Main";
+import Shopbandle from "./components/components_layout/shopbandle";
+import Footer from "./components/components_layout/Footer";
 
 export default function App() {
   return (
