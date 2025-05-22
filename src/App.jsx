550a856dd259c65ce9fcd00ecd876ba953d6ed04
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-22 18:03:39 +0200

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+// IMPORT 
+
 import Header from "./components/components_layout/Header";
 import Main from "./components/components_layout/Main";
 import Shopbandle from "./components/components_layout/shopbandle";
 import Footer from "./components/components_layout/Footer";
+
+
+// RUN UPP 
 
 export default function App() {
   return (
